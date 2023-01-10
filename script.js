cce5a61610423dcea4c93255277f332ae1d8ceec
zZEGORZz <somolot@bk.ru>
2023-01-10 19:06:14 +0300

    Learn Objects JS

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
-const mainForm = document.forms.main;
-function getCheck(){
-    const check = document.getElementById('choice2');
-    if(check.checked) alert('Правильно!');
-    else alert('Вы не угадали! Подумайте ещё!');
+// Edit testObject and Create objectTestClone
+const testObject = {
+    name: "qwerty",
+    surname: "Shekin"
 }
+
+testObject.age = 23;    
+debugger;
+
+const objectTestClone = {};
+Object.assign(objectTestClone, testObject);
+
+delete testObject.age;
+
+testObject.name = "Egor";
+debugger;
+
+
+//Count workers salary 
+let sum = 0;
+const salary = { 
+    Marie: 7,
+    Gleb: 8
+};
+salary.Egor = 10;
+
+if(salary === null) alert(sum);
+else{
+    for(i in salary){
+        sum += salary[i];
+    }
+    // alert(sum);
+}
+debugger;
+
+// X2 numbers in object 'menu'
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+function multiplyNumeric(menu){
+    for(i in menu){
+        if(typeof(menu[i]) ===  'number') menu[i] *= 2;
+    }
+}
+
+console.log(sum);
+
+multiplyNumeric(menu);
+debugger;
